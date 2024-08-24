a0c6f8f419979f950b812852c33e383dbffc01c9
Fil <sardmex@gmail.com>
2024-08-23 19:37:49 -0500

    Initial commit

---
+++ pages/articles/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Article Details</h1>
+    <p>Article ID: {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    return {
+      id: params.id,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Ваши стили для страницы */
+</style>
---
+++ pages/articles/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Articles</h1>
+      <ul>
+        <li v-for="article in articles" :key="article.id">
+          <NuxtLink :to="`/articles/${article.id}`">{{ article.title }}</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        articles: [
+          { id: 1, title: "Article 1" },
+          { id: 2, title: "Article 2" },
+        ],
+      };
+    },
+  };
+  </script>
+  
