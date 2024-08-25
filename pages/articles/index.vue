c2a716595e17ec7664572657b43f08d47c144711
Fil <sardmex@gmail.com>
2024-08-25 13:44:03 -0500

    push

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,32 +1,41 @@
 <template>
-  <div>
-    <h1>Articles</h1>
-    <ul>
-      <li v-for="article in articles" :key="article.id">
-        <nuxt-link :to="`/articles/${article.id}`">
-          {{ article.title }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </div>
-</template>
+    <div>
+      <h1>Статьи</h1>
+      <ul>
+        <li v-for="article in articles" :key="article.id">
+          <nuxt-link :to="`/articles/${article.id}`">{{ article.title }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { gql } from '@apollo/client/core';
+  import { useApollo } from '#app';
+  
+  const articles = ref([]);
+  
+  const query = gql`
+    query GetArticles {
+      articles {
+        id
+        title
+      }
+    }
+  `;
+  import apollo from "@/apollo/asrp-media";
 
-<script>
-import gql from 'graphql-tag'
+  import {QUERY_ARTICLES} from "@/apollo/asrp-media/queries";
 
-export default {
-  async asyncData({ app }) {
-    const { data } = await app.apolloProvider.defaultClient.query({
-      query: gql`
-        query {
-          articles {
-            id
-            title
-          }
-        }
-      `
-    })
-    return { articles: data.articles }
-  }
-}
-</script>
+const {data: {articles: {Articles}}} = await apollo.query({
+        query: QUERY_ARTICLES
+      });
+      
+  onMounted(async () => {
+    const { apollo } = useApollo();
+    const { data } = await apollo.query({ query });
+    articles.value = data.articles;
+  });
+  </script>
+  
